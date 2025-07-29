5ee8d50cd4f3389c8600e4957dc6257b90abb19c
Vishal-Verma84 <vermavishal9258@gmail.com>
2025-07-29 15:12:00 +0530

    Update Info.jsx
    miner update

--- src/Data/Info.jsx
+++ src/Data/Info.jsx
@@ -21,7 +21,7 @@ export const experience = [
         company : 'DaddyTech Solution',
         location : ' Gurugram',
         start : 'Apr 2023', 
-        end : 'Jun 2024 ',
+        end : 'Jan 2025 ',
         desc : 'Developed and implemented UI solutions based on user requirements and wireframes, ensuring seamless integration with the backend systems.',
         imgUrl : 'images/logo1.jpg'
     },
